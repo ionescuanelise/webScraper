1a9ae211dd086a3e005bed0e4d92aa179c2ebde0
anelise.maria <ionescu.anelise@gmail.com>
2022-02-26 15:35:49 +0000

    fixed bugs

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import csv
+import sys
 from time import sleep
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.common import exceptions
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common import exceptions
 
 
 def create_webdriver_instance():
@@ -13,7 +13,7 @@ def create_webdriver_instance():
     return driver
 
 
-def twitter_search(driver, search_term):
+def find_search_input_and_enter_criteria(search_term, driver):
     url = 'https://twitter.com/search'
     driver.get(url)
     driver.maximize_window()
@@ -31,18 +31,13 @@ def change_page_sort(tab_name, driver):
     tab = driver.find_element_by_link_text(tab_name)
     tab.click()
     xpath_tab_state = f'//a[contains(text(),\"{tab_name}\") and @aria-selected=\"true\"]'
-    return xpath_tab_state
 
 
 def generate_tweet_id(tweet):
     return ''.join(tweet)
 
 
-def scroll_down_page(driver, last_position, num_seconds_to_load=0.5, scroll_attempt=0, max_attempts=5):
-    """The function will try to scroll down the page and will check the current
-    and last positions as an indicator. If the current and last positions are the same after `max_attempts`
-    the assumption is that the end of the scroll region has been reached and the `end_of_scroll_region`
-    flag will be returned as `True`"""
+def scroll_down_page(driver, last_position, num_seconds_to_load=2, scroll_attempt=0, max_attempts=50):
     end_of_scroll_region = False
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sleep(num_seconds_to_load)
@@ -67,11 +62,6 @@ def save_tweet_data_to_csv(records, filepath, mode='a+'):
 
 
 def collect_all_tweets_from_current_view(driver, lookback_limit=25):
-    """The page is continously loaded, so as you scroll down the number of tweets returned by this function will
-     continue to grow. To limit the risk of 're-processing' the same tweet over and over again, you can set the
-     `lookback_limit` to only process the last `x` number of tweets extracted from the page in each iteration.
-     You may need to play around with this number to get something that works for you. I've set the default
-     based on my computer settings and internet speed, etc..."""
     page_cards = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
     if len(page_cards) <= lookback_limit:
         return page_cards
@@ -91,12 +81,6 @@ def extract_data_from_current_tweet_card(card):
     except exceptions.NoSuchElementException:
         handle = ""
     try:
-        """
-        If there is no post date here, there it is usually sponsored content, or some
-        other form of content where post dates do not apply. You can set a default value
-        for the postdate on Exception if you which to keep this record. By default I am
-        excluding these.
-        """
         postdate = card.find_element_by_xpath('.//time').get_attribute('datetime')
     except exceptions.NoSuchElementException:
         return
@@ -126,15 +110,16 @@ def extract_data_from_current_tweet_card(card):
     return tweet
 
 
-def main(search_term, filepath, max_items, page_sort='Latest'):
+def main(search_term, filepath, max_items, page_sort='Top'):
     save_tweet_data_to_csv(None, filepath, 'w')  # create file for saving records
     last_position = None
     end_of_scroll_region = False
     unique_tweets = set()
 
     driver = create_webdriver_instance()
-    twitter_search_page_term = twitter_search(driver, search_term)
-    if not twitter_search_page_term:
+
+    search_found = find_search_input_and_enter_criteria(search_term, driver)
+    if not search_found:
         return
 
     change_page_sort(page_sort, driver)
@@ -159,10 +144,7 @@ def main(search_term, filepath, max_items, page_sort='Latest'):
 
 
 if __name__ == '__main__':
-    list_crypto_coins = ['BTC', 'Solana', 'ShibainuCoin', 'ADA', 'LTC', 'XTZ', 'USDT', 'ATOM', 'Dogecoin', 'ETH']
-    for term in list_crypto_coins:
-        path = './data/'
-        Path(path).mkdir(exist_ok=True)
-        main(term, path + term, 10)
-
-
+    term = sys.argv[1]
+    path = './data/'
+    Path(path).mkdir(exist_ok=True)
+    main(term, path + term, 1000)
